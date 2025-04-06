692c3f3f6044995fb7272ec2096c26589bfe6fb7
om shankar <om8003golu@gmail.com>
2025-04-06 10:05:00 +0530

    create task in saperate modal

--- client/src/components/CreateTaskButton.jsx
+++ client/src/components/CreateTaskButton.jsx
@@ -1,36 +1,37 @@
 import React, { useState } from "react";
 import { PiPencilSimpleFill } from "react-icons/pi";
 import useCreateTask from "../hooks/useCreateTask";
+import CreateUpdateModal from "./CreateUpdateModal";
 
 const CreateTaskButton = () => {
   const [open, setOpen] = useState(false);
   const { loading, createTask } = useCreateTask();
-  const [taskData, setTaskData] = useState({
-    title: "",
-    description: "",
-    priority: "Low",
-    status: "pending",
-    category: "Others",
-  });
+  // const [taskData, setTaskData] = useState({
+  //   title: "",
+  //   description: "",
+  //   priority: "Low",
+  //   status: "pending",
+  //   category: "Others",
+  // });
 
-  const handleChange = (e) => {
-    setTaskData({ ...taskData, [e.target.name]: e.target.value });
-  };
+  // const handleChange = (e) => {
+  //   setTaskData({ ...taskData, [e.target.name]: e.target.value });
+  // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
 
-    createTask(taskData);
+  //   createTask(taskData);
 
-    setOpen(false);
-    setTaskData({
-      title: "",
-      description: "",
-      priority: "Low",
-      status: "pending",
-      category: "Others",
-    });
-  };
+  //   setOpen(false);
+  //   setTaskData({
+  //     title: "",
+  //     description: "",
+  //     priority: "Low",
+  //     status: "pending",
+  //     category: "Others",
+  //   });
+  // };
 
   return (
     <>
@@ -44,77 +45,14 @@ const CreateTaskButton = () => {
 
       {/* Modal */}
       {open && (
-        <div className="fixed inset-0 bg-black/30 backdrop-blur-sm  flex items-center justify-center z-50">
-          <div className="bg-white rounded-xl shadow-lg w-full max-w-md p-6 relative">
-            <button
-              onClick={() => setOpen(false)}
-              className="absolute top-3 right-3 cursor-pointer text-gray-500 hover:text-black"
-            >
-              ✕
-            </button>
-            <h2 className="text-xl font-semibold mb-4">Create New Task</h2>
-            <form onSubmit={handleSubmit} className="space-y-4">
-              <input
-                name="title"
-                placeholder="Title"
-                className="w-full border p-2 rounded"
-                value={taskData.title}
-                onChange={handleChange}
-                required
-              />
-              <textarea
-                name="description"
-                placeholder="Description"
-                className="w-full border p-2 rounded"
-                value={taskData.description}
-                onChange={handleChange}
-              />
-
-              <select
-                name="priority"
-                className="w-full border p-2 rounded"
-                value={taskData.priority}
-                onChange={handleChange}
-              >
-                <option value="Low">Low</option>
-                <option value="Medium">Medium</option>
-                <option value="High">High</option>
-              </select>
-              <select
-                name="status"
-                className="w-full border p-2 rounded"
-                value={taskData.status}
-                onChange={handleChange}
-              >
-                <option value="pending">Pending</option>
-                <option value="completed">Completed</option>
-              </select>
-              <select
-                name="category"
-                className="w-full border p-2 rounded"
-                value={taskData.category}
-                onChange={handleChange}
-              >
-                <option value="Others">Others</option>
-                <option value="Work">Work</option>
-                <option value="Personal">Personal</option>
-                <option value="Health">Health</option>
-                <option value="Finance">Finance</option>
-                <option value="Education">Education</option>
-                <option value="Shopping">Shopping</option>
-                <option value="Chores">Chores</option>
-                <option value="Meetings">Meetings</option>
-                <option value="Travel">Travel</option>
-              </select>
-              <button
-                type="submit"
-                className="w-full cursor-pointer bg-blue-600  hover:bg-blue-500 text-white p-2 rounded"
-              >
-                Create Task
-              </button>
-            </form>
-          </div>
-        </div>
+        <CreateUpdateModal
+          setOpen={setOpen}
+          purpose={"Create"}
+          modalFunction={createTask}
+          priority={"Low"}
+          status={"pending"}
+          category={"Others"}
+        />
       )}
     </>
   );
---
+++ client/src/components/CreateUpdateModal.jsx
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from "react";
+
+const CreateUpdateModal = ({
+  setOpen,
+  purpose,
+  modalFunction,
+  title,
+  description,
+  priority,
+  status,
+  category,
+}) => {
+  const [taskData, setTaskData] = useState({
+    title: "",
+    description: "",
+    priority: "",
+    status: "",
+    category: "",
+  });
+
+  useEffect(() => {
+    const initialiseData = () => {
+      (taskData.title = title),
+        (taskData.description = description),
+        (taskData.priority = priority),
+        (taskData.status = status),
+        (taskData.category = category);
+    };
+
+    initialiseData();
+  }, []);
+
+  const handleChange = (e) => {
+    setTaskData({ ...taskData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    modalFunction(taskData);
+    setOpen(false);
+    setTaskData({
+      title: "",
+      description: "",
+      priority: "",
+      status: "",
+      category: "",
+    });
+  };
+
+  return (
+    <>
+      <div className="fixed inset-0 bg-black/30 backdrop-blur-sm  flex items-center justify-center z-50">
+        <div className="bg-white rounded-xl shadow-lg w-full max-w-md p-6 relative">
+          <button
+            onClick={() => setOpen(false)}
+            className="absolute top-3 right-3 cursor-pointer text-gray-500 hover:text-black"
+          >
+            ✕
+          </button>
+          <h2 className="text-xl font-semibold mb-4">{purpose} New Task</h2>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <input
+              name="title"
+              placeholder="Title"
+              className="w-full border p-2 rounded"
+              value={taskData.title}
+              onChange={handleChange}
+              required
+            />
+            <textarea
+              name="description"
+              placeholder="Description"
+              className="w-full border p-2 rounded"
+              value={taskData.description}
+              onChange={handleChange}
+            />
+
+            <select
+              name="priority"
+              className="w-full border p-2 rounded"
+              value={taskData.priority}
+              onChange={handleChange}
+            >
+              <option value="Low">Low</option>
+              <option value="Medium">Medium</option>
+              <option value="High">High</option>
+            </select>
+            <select
+              name="status"
+              className="w-full border p-2 rounded"
+              value={taskData.status}
+              onChange={handleChange}
+            >
+              <option value="pending">Pending</option>
+              <option value="completed">Completed</option>
+            </select>
+            <select
+              name="category"
+              className="w-full border p-2 rounded"
+              value={taskData.category}
+              onChange={handleChange}
+            >
+              <option value="Others">Others</option>
+              <option value="Work">Work</option>
+              <option value="Personal">Personal</option>
+              <option value="Health">Health</option>
+              <option value="Finance">Finance</option>
+              <option value="Education">Education</option>
+              <option value="Shopping">Shopping</option>
+              <option value="Chores">Chores</option>
+              <option value="Meetings">Meetings</option>
+              <option value="Travel">Travel</option>
+            </select>
+            <button
+              type="submit"
+              className="w-full cursor-pointer bg-blue-600  hover:bg-blue-500 text-white p-2 rounded"
+            >
+              {purpose} Task
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CreateUpdateModal;
